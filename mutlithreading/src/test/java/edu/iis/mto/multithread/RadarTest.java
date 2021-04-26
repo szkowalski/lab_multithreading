180f674082f5327bd5fdc65d0a2c1c782f6aeab5
szymonkowalskiPL <74612883+szymonkowalskiPL@users.noreply.github.com>
2021-04-26 15:40:17 +0200

    Test for 0 rockets

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,11 +1,19 @@
 package edu.iis.mto.multithread;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import java.util.concurrent.ExecutorService;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 
 @ExtendWith(MockitoExtension.class)
 class RadarTest {
@@ -13,12 +21,23 @@ class RadarTest {
     @Mock
     private PatriotBattery batteryMock;
 
-    @Test
-    void launchPatriotOnceWhenNoticesAScudMissle() {
-        Radar radar = new Radar(batteryMock);
-        Scud enemyMissle = new Scud();
-        radar.notice(enemyMissle);
-        verify(batteryMock).launchPatriot(enemyMissle);
+    @Mock
+    private ExecutorService executorMock;
+
+    @BeforeEach
+    void startUp() {
+        when(executorMock.submit(any(Runnable.class))).thenAnswer(invocation -> {
+            ((Runnable) invocation.getArgument(0)).run();
+            return null;
+        });
     }
 
+    @RepeatedTest(10)
+    void launchNoRockets() {
+        BetterRadar radar = new BetterRadar(batteryMock, 0);
+        radar.executor = executorMock;
+        Scud enemyMissile = new Scud();
+        radar.notice(enemyMissile);
+        verify(batteryMock, times(0)).launchPatriot(enemyMissile);
+    }
 }
